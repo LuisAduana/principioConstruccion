edfad4f25b60230d183ef13b7f6bab5aade2fbb2
LuisAduana <38675549+LuisAduana@users.noreply.github.com>
2018-04-23 21:55:15 -0500

    Modulos
    División del sistema en 3 módulos, conexión a base de datos, interfaz gráfica y lógica de negocios

---
+++ controlAsistencias/src/logicaDeNegocios/ControlAsistencias.java
@@ -0,0 +1,16 @@
+package logicaDeNegocios;
+
+/**
+ * 
+ * @author Luis Bonilla
+ */
+public class ControlAsistencias {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
