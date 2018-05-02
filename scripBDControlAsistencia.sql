b131b9fd51f1b26252e3e6caaab9576e38633f08
LuisAduana <38675549+LuisAduana@users.noreply.github.com>
2018-05-02 16:18:49 -0500

    Scrip BD corregido
    Se eliminó la tabla del maestro que no era necesaria

--- scripBDControlAsistencia.sql
+++ scripBDControlAsistencia.sql
@@ -8,19 +8,14 @@ create table alumno(
     apeAlumnoMat varchar(50) not null,
     matricula varchar(9) not null,
     repite boolean not null,
-	asistencia int not null
-);
-
-create table docente(
-	idDocente int primary key auto_increment not null,
-    nombreDocente varchar(70) not null,
-    apeDocentePat varchar(50) not null,
-    apeDocenteMat varchar(50) not null,
-    numDocente varchar(10) not null
+	asistencia int not null,
+    id_experiencia int not null
 );
 
 create table experienciaEducativa(
 	idExperiencia int primary key auto_increment not null,
     nombreExperiencia varchar(300) not null,
     nrc varchar(5)
-)
+);
+
+alter table alumno add foreign key(id_experiencia) references experienciaeducativa(idExperiencia);
